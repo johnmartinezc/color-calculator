ceeb659288f8a9b35fc5b9dd5b09f6e6287008c1
johnmartinezc <john.martinez@codeimmersives.com>
2022-09-16 08:35:13 -0400

    -a

---
+++ main.js
@@ -0,0 +1,15 @@
+const prompt = require('prompt-sync')
+({sigint: true})
+
+
+let intro = ("Would you like to 1.combine or 2.deconstructcolors: ")
+console.log(intro)
+
+prompt(">")
+
+let combine = "1.combine"
+let deconsturct = "2.deconstructcolors"
+
+
+
+
